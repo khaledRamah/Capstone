231719a9d50c7efc0ef8fc3a51f3f75fe8da00cb
khaled gamal <gamalkhaled77@gmail.com>
2020-10-03 09:56:37 -0700

    add steps to install and start kubernetes

--- roles/create_instance/files/user_data.sh
+++ roles/create_instance/files/user_data.sh
@@ -8,3 +8,13 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo kubeadm init 
+
+sudo apt install docker.io -y
+sudo systemctl enable docker.service
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+kubectl taint nodes --all node-role.kubernetes.io/master-
